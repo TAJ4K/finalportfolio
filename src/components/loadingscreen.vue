713b5f50177fd85828b463788f31171159ad514a
TAJ4K <76080011+TAJ4K@users.noreply.github.com>
2022-09-21 10:20:08 -0400

    device checking for supported media

--- src/components/loadingscreen.vue
+++ src/components/loadingscreen.vue
@@ -5,12 +5,13 @@ https://dev.to/dd8888/how-to-create-a-simple-loading-screen-using-vue-4h7m
   <div
     class="absolute bg-cyan-700 h-full w-screen text-center z-[100] leading-[100vh]"
   >
-    <!-- <img
+    <img
       class="relative m-auto mt-[20%]"
       src="../../public/dialingprogress.gif"
       alt="loading..."
       width="250"
-    /> -->
+      v-if="isMobile"
+    />
     <video
       autoplay
       loop
@@ -20,6 +21,7 @@ https://dev.to/dd8888/how-to-create-a-simple-loading-screen-using-vue-4h7m
       alt="loading..."
       class="relative m-auto mt-[20%]"
       width="250"
+      v-else
     ></video>
   </div>
 </template>
@@ -29,6 +31,14 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "LoadingScreen",
+  data() {
+    return {
+      isMobile: false,
+    };
+  },
+  mounted() {
+    this.isMobile = window.innerWidth < 768;
+  },
 });
 </script>
 
